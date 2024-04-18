c6a959949786c4f9f070407d0736a9c4d6069697
Butxer93 <iktetxebarriaib@gmail.com>
2024-04-18 11:54:10 +0200

    List sort

---
+++ list-sort/main.py
@@ -0,0 +1,17 @@
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+
+tags.sort()
+
+print(tags) # ['code','development','python','tutorials']
+
+tags.sort(reverse=True)
+
+print(tags) # ['tutorials','development','python','code']
+
+totals = [234, 1, 543, 2345]
+
+totals.sort(reverse=True)
+
+print(totals) #  [2345, 543, 234, 1]
